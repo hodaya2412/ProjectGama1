6a1a38b12fe29e91fd7285d28b9f8a50df4e198c
hodaya <qwertyhodaya241273@gmail.com>
2025-09-02 00:23:30 +0300

    Add player characters and related assets to the scene

--- Game1/Assets/Script/TargetSpawner.cs
+++ Game1/Assets/Script/TargetSpawner.cs
@@ -4,10 +4,6 @@ public class TargetSpawner : MonoBehaviour
 {
     Rigidbody2D rb;
     float delayForDestroy = 0.1f;
-    //[SerializeField] bool isSpecial = true;
-    //private float horizontalMovementRange =1f; 
-    //private float MovementSpeed = 2f;
-    //private float startingX;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -16,20 +12,13 @@ public class TargetSpawner : MonoBehaviour
             Debug.LogError("Rigidbody2D is null!");
         }
 
-        //startingX = transform.position.x;
+       
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //if (isSpecial)
-        //{
-        //    float horizontalOffset = Mathf.Sin(Time.time * MovementSpeed) * horizontalMovementRange;
-
-        //    transform.position = new Vector2(startingX + horizontalOffset, transform.position.y);
-
-        //}
+    
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
